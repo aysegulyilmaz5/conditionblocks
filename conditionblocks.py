64aff663ac115917bcbc59c32a9e62cc14d39e3a
aysegulyilmaz5 <yilmazansr@gmail.com>
2021-07-13 19:11:36 +0300

    conditionblocks.py

---
+++ conditionblocks.py
@@ -0,0 +1,11 @@
+number1 = 10
+number2 = 20
+
+if number1 > number2:
+  print("Number1 greater than Number2")
+
+elif number1 < number2:
+  print("number1 less than number2")
+
+else:
+  print("number1 equals to number2")
---
+++ recapdemo1.py
@@ -0,0 +1,14 @@
+lights = ["red","yellow","green"]
+
+currentLight = lights[1]
+
+print(currentLight)
+
+if currentLight == "red":
+  print("STOP!")
+
+if currentLight == "yellow":
+  print("READY!")
+
+if currentLight == "green":
+  print("GO!")
